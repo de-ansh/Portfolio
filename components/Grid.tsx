a5cfa461ee3f14be3291228527a705380a1d72ed
Ashish <anshm9448@gmail.com>
2024-07-22 10:51:52 +0530

    first Commit

---
+++ components/Grid.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from './data'
+
+const Grid = () => {
+  return (
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map(({id, title, description, img, imgClassName , className, titleClassName, spareImg })=>(
+            <BentoGridItem title={title} id={id} key={id} description={description} className={className} img={img} imgClassName={imgClassName}
+            titleClassName={titleClassName} spareImg={spareImg}
+            />
+        ))}
+      </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
