5b6253532a1b3e0568eec2c864f1002ef6491cc6
Ashish Mishra <anshm9448@gmail.com>
2025-03-18 20:34:10 +0530

    social media links fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,16 +34,17 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Ashish Mishra
+          Copyright © 2025 Ashish Mishra
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
+            onClick={() => window.open(info.src, "_blank")}
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <img src={info.img} alt="icons" width={20} height={20}  />
             </div>
           ))}
         </div>
